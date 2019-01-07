2274a7c4d75e99462db7eefa97870cd55a689101
Jose Berrio <aldarisbm@gmail.com>
2019-01-07 14:18:48 -0600

    working changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './App.css';
 import Home from './components/Home.js';
 import Gallery from './components/Gallery.js'
-class App extends React.Component {
-  render() {
-    return <Gallery/>
-  }
-}
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import NavBar from './components/NavBar';
 
-export default App;
+export default class App extends React.Component {
+	render() {
+		return <Router>
+			<div className="dark-tint">
+				<NavBar />
+				<header className='header'>
+					<Route path='/' exact component={Home} />
+					<Route path='/Galeria' component={Gallery} />
+				</header>
+			</div>
+		</Router>
+	}
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import '../styles/Home.css';
-import NavBar from './NavBar'
 
 export default class Home extends React.Component {
-  render() {
-    return (
-      <div className="home">
-        <NavBar/>
-        <header className="home-header">
-          <h3>
-            Atenciones Berrio
-          </h3>
-          <a
-            className="App-link"
-            href="mailto:joseantonioberrio@hotmail.com?Subject=Presupuesto"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Contactanos
+	render() {
+		return (
+			<div>
+				<header className="home-header">
+					<h1>Atenciones Berrio</h1>
+					<a
+						className="App-link"
+						href="mailto:joseantonioberrio@hotmail.com?Subject=Presupuesto"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Contactanos
           </a>
-        </header>
-      </div>
-    );
-  }
+				</header>
+			</div>
+		);
+	}
 }
